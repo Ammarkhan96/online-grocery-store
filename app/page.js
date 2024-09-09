06d4a3e5028a4b53e891b4f821c2cfa5577538c9
Ammarkhan96 <ammarkhaan96@gmail.com>
2024-09-09 17:26:47 +0500

    first commit

--- app/page.js
+++ app/page.js
@@ -15,6 +15,9 @@ export default async function Home() {
       <Slider sliderList={sliderList} />
       <CategoryList categoryList={categoryList} />
       <ProductList productList={productList} />
+
+      <Image src="/banner.png" width={1000} height={300} alt="banner" 
+      className="w-full h-[400px] object-contain" />
       </div>
   );
 }
